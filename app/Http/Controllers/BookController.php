e0fc28f9efba0925b0e67d5f89f5d25ddf8db140
HECTOR HUGO <hhugoac@gmail.com>
2019-06-18 15:35:17 -0500

    Se agregaron los controladores a los modelos, con el crud para cada tabla.

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $books=Book::orderBy('id','DESC')->paginate(3);
+        return view('Libro.index',compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('Book.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request,['name'=>'required','author'=>'required','category_id'=>'required','publish_date'=>'required','user'=>'required']);
+        Book::create($request->all());
+        return redirect()->route('book.index')->with('success','Book added to the database');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $books=Book::find($id);
+        return view('book.view',compact('books'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $book=Book::find($id);
+        return view('book.edit',compact('book'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $this->validate($request, ['name'=>'required','author'=>'required','category_id'=>'required','publish_date'=>'required','user'=>'required']);
+ 
+        book::find($id)->update($request->all());
+        return redirect()->route('book.index')->with('success','Book updated!');
+ 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Book::find($id)->delete();
+        return redirect()->route('book.index')->with('success','Book deleted from the database!');
+    
+    }
+}
---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $categories=Category::orderBy('id','DESC')->paginate(3);
+        return view('category.index',compact('category')); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('Category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request,['name'=>'required','description'=>'required','manybooks'=>'required']);
+        Category::create($request->all());
+        return redirect()->route('category.index')->with('success','Category added to the table!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $categories=Category::find($id);
+        return view('category.view',compact('categories'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $category=Category::find($id);
+        return view('category.edit',compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $this->validate($request,['name'=>'required','description'=>'required','manybooks'=>'required']);
+ 
+        category::find($id)->update($request->all());
+        return redirect()->route('category.index')->with('success','Category updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        Category::find($id)->delete();
+        return redirect()->route('category.index')->with('success','Category deleted!');
+    }
+}
