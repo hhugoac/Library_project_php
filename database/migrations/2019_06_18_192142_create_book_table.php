246df1e47e6e2e7d75c586770aa264c6ff1a1271
HECTOR HUGO <hhugoac@gmail.com>
2019-06-18 14:32:22 -0500

    Se agregaron las tablas y las migraciones a la base de datos.

---
+++ database/migrations/2019_06_18_192142_create_book_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('author');
+            $table->integer('category_id')->references('id')
+                                ->on('categories')->onDelete('cascade');
+            $table->integer('publish_date');
+            $table->string('user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+        //
+    }
+}
