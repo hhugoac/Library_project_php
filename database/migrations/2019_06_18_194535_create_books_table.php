f526df55ec1009ee5b5a3d1c7e3efb44e00a3db6
HECTOR HUGO <hhugoac@gmail.com>
2019-06-18 14:48:20 -0500

    Se creo la relacion en la base de datos y se agregaron los modelos.

--- database/migrations/2019_06_18_194535_create_books_table.php
+++ database/migrations/2019_06_18_194535_create_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBookTable extends Migration
+class CreateBooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -17,12 +17,14 @@ class CreateBookTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('author');
-            $table->integer('category_id')->references('id')
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')
                                 ->on('categories')->onDelete('cascade');
             $table->integer('publish_date');
             $table->string('user');
             $table->timestamps();
         });
+        
     }
 
     /**
