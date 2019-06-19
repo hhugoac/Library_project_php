b8d8cfc46b1eb2821cdbaf2aac578df9d7363388
HECTOR HUGO <hhugoac@gmail.com>
2019-06-19 08:05:08 -0500

    Se agregarn las rutas.

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::resource('book','BookController');
+Route::resource('category','CategoryController');
 Route::get('/', function () {
     return view('welcome');
 });
